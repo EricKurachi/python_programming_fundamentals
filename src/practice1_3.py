a34d792fdd4f212985270916c8766dd7b327a979
Eric Kurachi <erictkurachi@gmail.com>
2020-05-15 04:10:48 -0300

    practice 1.3 works, but not for any binary

---
+++ src/practice1_3.py
@@ -0,0 +1,14 @@
+""" Find the negative of a binary number """
+
+binary_number = 0b01010011
+binary_number_str = '01010011'
+
+# Using 2's complement
+
+complement = ''
+
+for digit in binary_number_str:
+    complement += str((int(digit) + 1) % 2)
+complement = int(complement) + 1
+
+print(complement)
