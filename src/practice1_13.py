e8004d6fa6c213f676c5ed9dd5b108dd2095052b
Eric Kurachi <erictkurachi@gmail.com>
2020-05-16 17:26:22 -0300

    practice 1.13

---
+++ src/practice1_13.py
@@ -0,0 +1,9 @@
+"""
+Assume that we want to pause our program to display some
+output and we want to let the user press some key to continue. We want to print
+“press any key to continue. . .” to the screen. Can we use the input function to
+implement this? If so, how would you write the input statement? If not, why
+can’t you use input?
+"""
+
+# We can't use because input needs the user to press enter
