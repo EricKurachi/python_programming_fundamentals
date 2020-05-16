d1aa0e17061590f4f302529c82b096fe24b58a70
Eric Kurachi <erictkurachi@gmail.com>
2020-05-16 17:21:49 -0300

    practice 1.12

---
+++ src/practice1_12.py
@@ -0,0 +1,16 @@
+"""
+Change the program above to convert 83 to its ASCII character
+equivalent. Save the value in a variable and print the following to the screen in
+the exact format you see here.
+
+The ASCII character equivalent of 83 is S
+"""
+
+x = str(83)
+print(x[0])
+print(x[1])
+y = int(x)
+print(y)
+
+z = chr(y)
+print('The ASCII character equivalent of 83 is', z)
