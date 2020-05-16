a15de59a6fcc9457a3b5e4040b5c3dcf3c888151
Eric Kurachi <erictkurachi@gmail.com>
2020-05-16 17:09:04 -0300

    practice 1.11

---
+++ src/practice1_11.py
@@ -0,0 +1,10 @@
+"""
+Write the three line program given in the two listings on p. 24.
+Then, without writing the string literal “house”, modify it to print the string
+“house” to the screen using string indexing. HINT: You can add strings together
+to build a new string.
+"""
+
+name = 'Sophus Lie'
+
+print(name[3] + name[1] + name[4:6] + name[-1])
