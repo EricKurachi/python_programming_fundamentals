e547f7cab28647e0cd4c190029e9c70075b514ba
Eric Kurachi <erictkurachi@gmail.com>
2020-05-19 22:45:03 -0300

    exercise 1.3

--- src/Chapter 1/Exercises/exercise_3.py
+++ src/Chapter 1/Exercises/exercise_3.py
@@ -6,7 +6,11 @@ drove your car and the number of gallons of gas you put in your car and then
 prints the miles per gallon you got on that tank of gas. Here is a sample run of
 the program.
 Please enter the miles you drove : 256
-Please enter the gallons of gas you put i n the tank : 10.1
+Please enter the gallons of gas you put in the tank : 10.1
 You got 25.346534653465348 mpg on that tank of gas.
 """
 
+miles_drove = float(input("Please enter the miles you drove: "))
+gallons = float(input("Please enter the gallons of gas you put in the tank: "))
+
+print("You got {} mpg on that tank of gas.".format(miles_drove/gallons))
