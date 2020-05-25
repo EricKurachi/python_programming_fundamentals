162f8474716ba74440bcab9840d2e73cc787eea8
Eric Kurachi <erictkurachi@gmail.com>
2020-05-25 16:18:08 -0300

    solved practice 2.1

---
+++ src/Chapter 2/Practice/practice_1.py
@@ -0,0 +1,16 @@
+"""
+Write a short program that asks the user to enter the name of
+a month. If the user enters “December” your program should print “Merry
+Christmas!”. No matter what you enter, your program should print “Have a
+Happy New Year!” just before the program terminates. Then, use Step Into and
+Step Over to execute each statement that you wrote. Run your program at least
+twice to see how it behaves when you enter “December” and how it behaves
+when you enter something else.
+"""
+
+chosen_month = input("please enter the name of a month: ")
+
+if chosen_month == "December":
+    print("Merry Christmas!")
+
+print("Have a Happy New Year!")
