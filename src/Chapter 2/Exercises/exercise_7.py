e8235b5e42aff824ae8c2b0f9cf75a848dc4aede
Eric Kurachi <erictkurachi@gmail.com>
2020-06-01 03:29:21 -0300

    solved exercise 2.7

--- src/Chapter 2/Exercises/exercise_7.py
+++ src/Chapter 2/Exercises/exercise_7.py
@@ -14,35 +14,38 @@ then add 1 to the integer and negate the result to get the 2’s complement.
 The conversion from bits to an integer can be carried out by multiplying each bit
 by the power of 2 that it represents as described in Sect.1.5 of Chap. 1
 """
+flag = ''
 
 binary_number = input("Please enter a 16-bit binary number: ")
-if len(binary_number) == 16 and binary_number[0] == '1':
-    binary_number = -((1111111111111111 - int(binary_number)) + 1)
+if len(binary_number) != 16:
+    print("Wrong size")
+    exit(0)
 
-binary_number = str(binary_number)
-print(binary_number)
-try:
-    decimal = int(binary_number[-1]) * (2 ** 0)
-    decimal += int(binary_number[-2]) * (2 ** 1)
-    decimal += int(binary_number[-3]) * (2 ** 2)
-    decimal += int(binary_number[-4]) * (2 ** 3)
-    decimal += int(binary_number[-5]) * (2 ** 4)
-    decimal += int(binary_number[-6]) * (2 ** 5)
-    decimal += int(binary_number[-7]) * (2 ** 6)
-    decimal += int(binary_number[-8]) * (2 ** 7)
-    decimal += int(binary_number[-9]) * (2 ** 8)
-    decimal += int(binary_number[-10]) * (2 ** 9)
-    decimal += int(binary_number[-11]) * (2 ** 10)
-    decimal += int(binary_number[-12]) * (2 ** 11)
-    decimal += int(binary_number[-13]) * (2 ** 12)
-    decimal += int(binary_number[-14]) * (2 ** 13)
-    decimal += int(binary_number[-15]) * (2 ** 14)
-    decimal += int(binary_number[-16]) * (2 ** 15)
-    decimal = binary_number[-17] + decimal
-except IndexError:
-    print("Positive decimal")
-except ValueError:
-    decimal = '-' + decimal
+if binary_number[0] == '1':
+    binary_number = (1111111111111111 - int(binary_number))
+    flag = 'negative'
+
+binary_number = str(binary_number).zfill(16)
+
+decimal = int(binary_number[0]) * (2 ** 15)
+decimal += int(binary_number[1]) * (2 ** 14)
+decimal += int(binary_number[2]) * (2 ** 13)
+decimal += int(binary_number[3]) * (2 ** 12)
+decimal += int(binary_number[4]) * (2 ** 11)
+decimal += int(binary_number[5]) * (2 ** 10)
+decimal += int(binary_number[6]) * (2 ** 9)
+decimal += int(binary_number[7]) * (2 ** 8)
+decimal += int(binary_number[8]) * (2 ** 7)
+decimal += int(binary_number[9]) * (2 ** 6)
+decimal += int(binary_number[10]) * (2 ** 5)
+decimal += int(binary_number[11]) * (2 ** 4)
+decimal += int(binary_number[12]) * (2 ** 3)
+decimal += int(binary_number[13]) * (2 ** 2)
+decimal += int(binary_number[14]) * (2 ** 1)
+decimal += int(binary_number[15]) * (2 ** 0)
+
+if flag == 'negative':
+    decimal = '-' + str(decimal + 1)
     print("Negative decimal")
 
 print(decimal)
