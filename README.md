fd03aa8a83cc26d4ad20d5a137b814e76260884a
Eric <erictkurachi@gmail.com>
2020-06-06 03:17:44 -0300

    Solved practice 3.1

--- README.md
+++
@@ -1,2 +0,0 @@
-# python_programming_fundamentals
-Solutions of the book "Python Programming Fundamentals" by Kent D. Lee
--- src/Chapter 3/Practice/practice_1.py
+++ src/Chapter 3/Practice/practice_1.py
@@ -3,3 +3,7 @@ Write a short program that asks the user to enter a sentence. Then
 print the sentence back to the screen with all lower case letters capitalized and
 all upper case letters in lower case.
 """
+
+sentence = input("Please enter a sentence: ")
+
+print(sentence.swapcase())
