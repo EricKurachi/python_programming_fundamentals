3eada4aa5628d610dda154620c4304099aea552d
Eric <erictkurachi@gmail.com>
2020-06-08 17:55:05 -0300

    Solved practice 3.14

---
+++ src/Chapter 3/Practice/practice_14.py
@@ -0,0 +1,9 @@
+"""
+eliminate the extra newline character at the end of each line.
+"""
+
+filename = input("Please enter the name of a file: ")  # Use file.txt for example
+catfile = open(filename, "r")
+for line in catfile:
+    print(line.rstrip())
+catfile.close()
