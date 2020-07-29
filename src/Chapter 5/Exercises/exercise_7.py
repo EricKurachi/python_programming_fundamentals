eaa3db0d518d439ebb92adcc8936d6b59b69301d
Eric Kurachi <erictkurachi@gmail.com>
2020-07-29 18:41:10 -0300

    Finished exercise 5.7

--- src/Chapter 5/Exercises/exercise_7.py
+++ src/Chapter 5/Exercises/exercise_7.py
@@ -3,3 +3,16 @@ Write a function called isPrime that returns True if an integer given to the fun
 is a prime number.
 """
 
+def is_prime(n):
+    max_divisor = int(n**(1/2))
+    for i in range(2, max_divisor):
+        if n % i == 0:
+            return False
+    return True
+
+def main():
+    n = int(input("Please enter a number: "))
+    print(is_prime(n))
+
+if __name__ == "__main__":
+    main()
