fef92014910ea95db78e8a65b5034d747e6e644f
Eric Kurachi <erictkurachi@gmail.com>
2020-07-31 23:20:04 -0300

    Finished exercise 5.14

--- src/Chapter 5/Exercises/exercise_14.py
+++ src/Chapter 5/Exercises/exercise_14.py
@@ -4,4 +4,14 @@ of that integer.
 """
 
 def factors(n):
-    
+    i = 1
+    factors_list = []
+    while i <= n/2:
+        if n % i == 0:
+            factors_list.append(i)
+        i += 1
+    factors_list.append(n)
+    return factors_list
+
+if __name__ == "__main__":
+    print(factors(6))
